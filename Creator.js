db2deaf3ff9fce20d905b1477243310438eb7d89
24hoster <94149524+24hoster@users.noreply.github.com>
2022-01-09 08:39:51 +0200

    Add files via upload

---
+++ Creator.js
@@ -0,0 +1,47 @@
+const colours = {
+    reset: "\x1b[0m",
+    bright: "\x1b[1m",
+    dim: "\x1b[2m",
+    underscore: "\x1b[4m",
+    blink: "\x1b[5m",
+    reverse: "\x1b[7m",
+    hidden: "\x1b[8m",
+    
+    fg: {
+        black: "\x1b[30m",
+        red: "\x1b[31m",
+        green: "\x1b[32m",
+        yellow: "\x1b[33m",
+        blue: "\x1b[34m",
+        magenta: "\x1b[35m",
+        cyan: "\x1b[36m",
+        white: "\x1b[37m",
+        crimson: "\x1b[38m" 
+    },
+    bg: {
+        black: "\x1b[40m",
+        red: "\x1b[41m",
+        green: "\x1b[42m",
+        yellow: "\x1b[43m",
+        blue: "\x1b[44m",
+        magenta: "\x1b[45m",
+        cyan: "\x1b[46m",
+        white: "\x1b[47m",
+        crimson: "\x1b[48m"
+    }
+};
+
+(async  function() {
+    while (true) {
+        await sleep(await Creator(""));
+    }
+})();
+
+function Creator(Proxylessless) {
+    return new Promise(async function(resolve) {
+        console.clear()
+        console.log(colours.fg.white, "" ,colours.fg.crimson,"                                       ╔═════════════════════════════════════╗") ;
+        console.log(colours.fg.white, "" ,colours.fg.cyan,"                                              ╔╦╗   ╔═╗┬─┐┌─┐┌─┐┌┬┐┌─┐┬─┐") ;
+        console.log(colours.fg.white, "" ,colours.fg.white,"                                               ║║───║  ├┬┘├┤ ├─┤ │ │ │├┬┘") ;
+        console.log(colours.fg.white, "" ,colours.fg.magenta,"                                              ═╩╝   ╚═╝┴└─└─┘┴ ┴ ┴ └─┘┴└─") ;
+        console.log(colours.fg.white, "" ,colours.fg.crimson,"                                       ╚═════════════════════════════════════╝ ") ;
